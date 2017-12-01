90e496550bfb27f88adc43e06417ab5c73035003
jackyu <455501914@qq.com>
2017-12-01 15:34:40 +0800

    change readme

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-## SSD License Plate Detection
+## Mobilenet-SSD License Plate Detection & Recognition
 This is a project for license plate detection using mobilenet-ssd.
 - Accuracy : 86.7%.
-- Speed : 280ms on 720P images.
+- Speed : 180ms(multi-cores) on 720P images.(OpenCV 3.3)
 
 ## Install
-- Caffe-SSD or Opencv 3.3+
+- Caffe-SSD or Opencv 3.3
+
+##### Note: 使用OpneCV 3.3 中的DNN模块 无需安装Caffe.
 
 ## Results
+
 ![](images/2.png)
+
 ![](images/4.png)
+
 ![](images/5.png)
+
+###  Recognition Demo
+
+简单的结合HyperLPR中的端到端模型，可以简单的实现车牌识别:
+
+![r1](/Users/yujinke/me/SSD_License_Plate_Detection/images/r2.png)
+
+![r1](/Users/yujinke/me/SSD_License_Plate_Detection/images/r1.png)
---
+++ detect_opencv33.py
@@ -0,0 +1,79 @@
+#coding=utf-8
+from cv2 import dnn
+import cv2
+import e2e
+
+inWidth = 720
+inHeight = 1024
+WHRatio = inWidth / float(inHeight)
+inScaleFactor = 0.007843
+meanVal = 127.5
+
+classNames = ('background',
+              'plate')
+net = dnn.readNetFromCaffe("MobileNetSSD_test.prototxt","lpr.caffemodel")
+
+import time
+
+def detect(cpp):
+    frame = cv2.imread(cpp)
+    blob = dnn.blobFromImage(frame, inScaleFactor, (inWidth, inHeight), meanVal)
+    net.setInput(blob)
+    t0 = time.time()
+    detections = net.forward()
+    print time.time() - t0
+
+    cols = frame.shape[1]
+    rows = frame.shape[0]
+
+    if cols / float(rows) > WHRatio:
+        cropSize = (int(rows * WHRatio), rows)
+    else:
+        cropSize = (cols, int(cols / WHRatio))
+
+    y1 = (rows - cropSize[1]) / 2
+    y2 = y1 + cropSize[1]
+    x1 = (cols - cropSize[0]) / 2
+    x2 = x1 + cropSize[0]
+    frame = frame[y1:y2, x1:x2]
+
+    cols = frame.shape[1]
+    rows = frame.shape[0]
+
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.2:
+            class_id = int(detections[0, 0, i, 1])
+
+            xLeftBottom = int(detections[0, 0, i, 3] * cols)
+            yLeftBottom = int(detections[0, 0, i, 4] * rows)
+            xRightTop = int(detections[0, 0, i, 5] * cols)
+            yRightTop = int(detections[0, 0, i, 6] * rows)
+
+            cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
+                          (0, 255, 255))
+
+            image_sub = frame[yLeftBottom:yRightTop,xLeftBottom:xRightTop]
+            print yLeftBottom,yRightTop, xLeftBottom,xRightTop
+            # e2e.recognizeOne(image_sub)
+
+            label = classNames[class_id] + ": " + str(confidence)
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+            # cv2.rectangle(frame, (xLeftBottom, yLeftBottom - labelSize[1]),
+            #               (xLeftBottom + labelSize[0], yLeftBottom + baseLine),
+            #               (255, 255, 255), 2,cv2.FILLED)
+            cv2.putText(frame, label, (xLeftBottom, yLeftBottom),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+    return frame
+import os
+
+
+
+test_dir = "test_folder"
+
+for f in os.listdir(test_dir):
+    if f.endswith(".jpg"):
+        cv2.imshow("test",detect(test_dir + "/" + f))
+        cv2.waitKey(0)
+        # break
